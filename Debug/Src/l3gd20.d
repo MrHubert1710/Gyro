1864579eb57b7dd5ca81f05eb1f6bc7f2d2ff6ba
MrHubert1710 <dylek.pl@gmail.com>
2019-05-08 09:58:04 +0200

    Konfiguracja

---
+++ Debug/Src/l3gd20.d
@@ -0,0 +1,7 @@
+Src/l3gd20.o: ../Src/l3gd20.c \
+ C:/Users/Hubert/Desktop/Gyro/Gyro/Inc/l3gd20.h \
+ C:/Users/Hubert/Desktop/Gyro/Gyro/Inc/gyro.h
+
+C:/Users/Hubert/Desktop/Gyro/Gyro/Inc/l3gd20.h:
+
+C:/Users/Hubert/Desktop/Gyro/Gyro/Inc/gyro.h:
